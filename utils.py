395ddc8628393a655769b46c1fedd250f2753de1
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-26 17:05:48 -0400

    TCP Packet packing sorta complete

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-import random, socket, struct
+import random, socket
+from struct import pack, unpack
 
 
 """ 
@@ -42,8 +43,17 @@ import random, socket, struct
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 """
 
+""" Helper method to retrieve the localhost address and port """
+def get_localhost():
+    host_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host_socket.connect(('8.8.8.8', 80))
+    
+    # Returns the localhost name: (IP Address, Port No.)
+    return host_socket.getsockname()
+
 """ Set of constant fields """
 HTTP_STATUS_CODE = 200
+FORMAT = 'UTF-8'
 
 """ TCP Header fields """
 # * https://www.quora.com/When-using-a-localhost-how-many-ports-are-there
@@ -56,6 +66,7 @@ TCP_DATA_OFFSET = 5  # (No. of words = No. of rows). Offset to show after where 
 TCP_ADV_WINDOW = 5840  # TCP header value allocated for window size: two bytes long. Highest numeric value for a receive window is 65,535 bytes.
 TCP_CHECKSUM = 0
 TCP_URGENT_PTR = 0
+TCP_CWND = 1
 
 # * https://www.howtouselinux.com/post/tcp-flags#:~:text=TCP%20flags%20are%20various%20types,%2C%20fin%2C%20urg%2C%20psh.
 """ TCP Flags """
@@ -67,20 +78,23 @@ FLAG_TCP_URG = 0 # Urgent
 FLAG_TCP_PSH = 0 # Push
 TCP_FLAGS = FLAG_TCP_FIN + (FLAG_TCP_SYN << 1) + (FLAG_TCP_RST << 2) + (FLAG_TCP_PSH << 3) + (FLAG_TCP_ACK << 4) + (FLAG_TCP_URG << 5)    # << i: 2^i
 
-""" TCP Header formats """
+
+""" IP Header fields """
+# Convert IP addr dotted-quad string into 32 bit binary format
+# * https://pythontic.com/modules/socket/inet_aton
+IP_SRC_ADDRESS = socket.inet_aton(get_localhost()[0])
+IP_DEST_ADDRESS = socket.inet_aton('')    # TODO: Extract Dest IP addr from the input argument URL
+IP_PROTOCOL = socket.IPPROTO_TCP
+IP_PADDING = 0
+
+""" Header formats """
 TCP_HEADER_FORMAT = '!HHLLBBHHH'
+TCP_HEADER_SEGMENT_FORMAT = '!HHLLBBH'
+PSEUDO_IP_HEADER_FORMAT = '!4s4sBBH'
+
+# TODO: Start with IP Header information (Fields + Flags + IP Data packing)
 
 
-''' CheckSum of the TCP is calculated by taking into account TCP header, TCP body and Pseudo IP header
-    * Cannot correctly guess the IP header size from Transport layer
-    * Calculate checksum using part of the IP header info that will remain unchanged in every packet
-    * Pseudo IP Header fields:
-        1. IP of Source and Destination
-        2. TCP/UDP Segment length
-        3. Protocol (type of protocol)
-        4. Padding (8 bits)
-    * Only used for TCP Checksum calculation, discarded later and not sent to the Network layer
-'''
 
 """ Helper method to calculate checksum """
 ''' Refereced from Suraj Singh, Bitforestinfo '''
@@ -98,17 +112,47 @@ def calc_header_checksum(header_data):
     binary_checksum += (binary_checksum >> 16)
     return ~binary_checksum & 0xffff
 
-""" Helper method to retrieve the localhost address and port """
-def get_localhost():
-    host_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    host_socket.connect(('8.8.8.8', 80))
-    
-    # Returns the localhost name: (IP Address, Port No.)
-    return host_socket.getsockname()
 
-""" Helper method to instantiate TCP fields """
-def pack_tcp_fields(seq_num: int, ack_num: int, adv_window: int, data: str):
-    tcp_header = struct.pack(
+""" 
+Helper method to instantiate TCP fields: Takes in TCP fields as params that do not remain constant and pack with the data
+    1. param: seq_num - sequence number of the current packet
+    2. param: ack_num - acknowledgement number of last ACKed packet
+    3. param: flags - flags that will be changed (SYN, ACK, FIN)
+    4. param: adv_window - current advertised window of the receiver # ? Re-evaluate whether we need it
+    5. param: data - payload to be send over the raw socket connection
+"""
+# ? Check if we can split pack and re-pack functions for TCP fields
+def pack_tcp_fields(seq_num: int, ack_num: int, flags: int, adv_window: int, data: str):
+    tcp_header = pack(
         TCP_HEADER_FORMAT, 
         TCP_SOURCE_PORT, TCP_DEST_PORT, seq_num, ack_num, TCP_DATA_OFFSET, TCP_FLAGS, adv_window, TCP_CHECKSUM, TCP_URGENT_PTR
     )
+
+    tcp_segment_length = len(tcp_header) + len(data)
+
+    ''' CheckSum of the TCP is calculated by taking into account TCP header, TCP body and Pseudo IP header
+        * Cannot correctly guess the IP header size from Transport layer
+        * Calculate checksum using part of the IP header info that will remain unchanged in every packet
+        * Pseudo IP Header fields (in order):
+            1. IP of Source and Destination
+            2. Padding/Placeholder (8 bits)
+            3. Protocol (type of protocol)
+            4. TCP/UDP Segment length
+        * Only used for TCP Checksum calculation, discarded later and not sent to the Network layer
+    '''
+    pseudo_ip_header = pack(
+        PSEUDO_IP_HEADER_FORMAT, 
+        IP_SRC_ADDRESS, IP_DEST_ADDRESS, IP_PADDING, IP_PROTOCOL, tcp_segment_length
+    )
+
+    # Calculate Checksum by taking into account TCP header, TCP body and Pseudo IP header
+    checksum = calc_header_checksum(tcp_header + data.encode() + pseudo_ip_header)
+
+    # Repack TCP header
+    tcp_header = pack(
+        TCP_HEADER_SEGMENT_FORMAT, 
+        TCP_SOURCE_PORT, TCP_DEST_PORT, seq_num, ack_num, TCP_DATA_OFFSET, TCP_FLAGS, adv_window
+    ) + pack('H', checksum) + pack('H', TCP_URGENT_PTR)
+
+    tcp_packet = tcp_header + data.encode(FORMAT)
+    return tcp_packet
