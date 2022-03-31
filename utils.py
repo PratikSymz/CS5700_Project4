78ea37a1e05476dfd3d0cb3e929840f9a38a35dc
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-31 14:57:24 -0400

    IP in progress...

--- utils.py
+++ utils.py
@@ -85,19 +85,27 @@ TCP_FLAGS = FLAG_TCP_FIN + (FLAG_TCP_SYN << 1) + (FLAG_TCP_RST << 2) + (FLAG_TCP
 """ IP Header fields """
 # Convert IP addr dotted-quad string into 32 bit binary format
 # * https://pythontic.com/modules/socket/inet_aton
-IP_SRC_ADDRESS = socket.inet_aton(get_localhost()[0])
-IP_DEST_ADDRESS = socket.inet_aton('')    # TODO: Extract Dest IP addr from the input argument URL
-IP_PROTOCOL = socket.IPPROTO_TCP
-IP_PADDING = 0
 IP_VERSION = 4
 IP_HEADER_LEN = 5
-IP_VHL = (IP_VERSION << 4) + IP_HEADER_LEN
 IP_TOS = 0
+IP_DGRAM_LEN = 20     # Start with IHL -> 5 words -> 20B + DATA Length (not known yet)
 IP_ID = 54321
-IP_TOT_LEN = 255
+IP_TTL = 255
+IP_PROTOCOL = socket.IPPROTO_TCP
+IP_CHECKSUM = 0
+IP_SRC_ADDRESS = socket.inet_aton(get_localhost()[0])
+IP_DEST_ADDRESS = socket.inet_aton('')    # TODO: Extract Dest IP addr from the input argument URL
+IP_PADDING = 0
+IP_VER_HEADER_LEN = (IP_VERSION << 4) + IP_HEADER_LEN
 
 """ IP Flags """
-FRAG_OFFSET = 0
+# TODO: Remove other flags except FRAG_OFFSET if not needed
+FLAG_IP_RSV = 0
+FLAG_IP_DTF = 0
+FLAG_IP_MRF = 0
+FLAG_IP_FRAG_OFFSET = 0
+IP_FLAGS = (FLAG_IP_RSV << 7) + (FLAG_IP_DTF << 6) + (FLAG_IP_MRF << 5) + (FLAG_IP_FRAG_OFFSET)
+
 
 """ Header formats """
 # '!' - Network packet order
@@ -107,11 +115,11 @@ PSEUDO_IP_HEADER_FORMAT = '!4s4sBBH'
 IP_HEADER_FORMAT = '!BBHHHBB'
 IP_HEADER_SEGMENT_FORMAT = '!4s4s'
 
-# TODO: Start with IP Header information (Fields + Flags + IP Data packing)
-
+# TODO: Start with IP Header information (P Data packing)
 """ IP and TCP header field keys """
 KEYS_TCP_FIELDS = ['src_port', 'dest_port', 'seq_num', 'ack_num', 'data_offset', 'flags', 'adv_window', 'checksum', 'urgent_ptr']
 KEYS_IP_FIELDS = ['version', 'vhl', 'tos', 'total_len', 'id', 'flags', 'frag_offset', 'ttl', 'protocol', 'checksum', 'src_addr', 'dest_addr']
+
 
 """ Helper method to calculate checksum """
 ''' Refereced from Suraj Singh, Bitforestinfo '''
@@ -130,13 +138,13 @@ def compute_header_checksum(header_data):
     return ~binary_checksum & 0xffff
 
 """ Helper method to verify TCP checksum """
-def validate_header_checksum(packet_checksum, tcp_fields, tport_layer_packet, tcp_options, net_layer_packet):
+def validate_header_checksum(packet_checksum, tcp_fields, tport_layer_packet, tcp_options, payload):
     tcp_header = pack(
         TCP_HEADER_SEGMENT_FORMAT, 
         tcp_fields['src_port'], tcp_fields['dest_port'], tcp_fields['seq_num'], tcp_fields['ack_num'], tcp_fields['data_offset'], tcp_fields['flags'], tcp_fields['adv_window'], tcp_fields['checksum'], tcp_fields['urgent_ptr']
     ) + tcp_options  # TCP Options wasn't unpacked hence, no need to be packet again
 
-    tcp_segment_length = len(tport_layer_packet)
+    tcp_segment_length = len(tport_layer_packet)    # Already contains payload
     pseudo_ip_header = pack(
         PSEUDO_IP_HEADER_FORMAT, 
         IP_SRC_ADDRESS, IP_DEST_ADDRESS, IP_PADDING, IP_PROTOCOL, tcp_segment_length
@@ -144,7 +152,7 @@ def validate_header_checksum(packet_checksum, tcp_fields, tport_layer_packet, tc
 
     # Calculate Checksum by taking into account TCP header, TCP body and Pseudo IP header
     # ? Do I need to create TCP headers again to compute checksum
-    return (packet_checksum == compute_header_checksum(tcp_header + net_layer_packet.encode(FORMAT) + pseudo_ip_header))
+    return (packet_checksum == compute_header_checksum(tcp_header + payload.encode(FORMAT) + pseudo_ip_header))
 
 """ 
 Helper method to instantiate TCP fields: Takes in TCP fields as params that do not remain constant and pack with the data
@@ -156,15 +164,15 @@ Helper method to instantiate TCP fields: Takes in TCP fields as params that do n
     6. return: Data packet with the TCP header added on top of the IP header and the payload (data)
 """
 # ? Check if we can split pack and re-pack functions for TCP fields
-def pack_tcp_fields(seq_num: int, ack_num: int, flags: int, adv_window: int, net_layer_data: str):
-    tcp_header = pack(
+def pack_tcp_fields(seq_num: int, ack_num: int, flags: int, adv_window: int, payload: str):
+    temp_tcp_header = pack(
         TCP_HEADER_FORMAT, 
         TCP_SOURCE_PORT, TCP_DEST_PORT, seq_num, ack_num, TCP_DATA_OFFSET, flags, adv_window, TCP_CHECKSUM, TCP_URGENT_PTR
     )
 
-    tcp_segment_length = len(tcp_header) + len(net_layer_data)
+    tcp_segment_length = len(temp_tcp_header) + len(payload)
 
-    ''' CheckSum of the TCP is calculated by taking into account TCP header, TCP body and Pseudo IP header
+    ''' Checksum of the TCP is calculated by taking into account TCP header, TCP body and Pseudo IP header
         * Cannot correctly guess the IP header size from Transport layer
         * Calculate checksum using part of the IP header info that will remain unchanged in every packet
         * Pseudo IP Header fields (in order):
@@ -180,7 +188,7 @@ def pack_tcp_fields(seq_num: int, ack_num: int, flags: int, adv_window: int, net
     )
 
     # Calculate Checksum by taking into account TCP header, TCP body and Pseudo IP header
-    checksum = compute_header_checksum(tcp_header + net_layer_data.encode(FORMAT) + pseudo_ip_header)
+    checksum = compute_header_checksum(temp_tcp_header + payload.encode(FORMAT) + pseudo_ip_header)
 
     # Repack TCP header
     tcp_header = pack(
@@ -188,7 +196,7 @@ def pack_tcp_fields(seq_num: int, ack_num: int, flags: int, adv_window: int, net
         TCP_SOURCE_PORT, TCP_DEST_PORT, seq_num, ack_num, TCP_DATA_OFFSET, TCP_FLAGS, adv_window
     ) + pack('H', checksum) + pack('!H', TCP_URGENT_PTR)
 
-    tcp_packet = tcp_header + net_layer_data.encode(FORMAT)
+    tcp_packet = tcp_header + payload.encode(FORMAT)
     return tcp_packet
 
 """ 
@@ -199,11 +207,11 @@ Helper method to unpack TCP fields: Takes in Transport layer packet as param and
 def unpack_tcp_fields(tport_layer_packet):
     # Extract header fields from packet - 5 words - 20B. After 20B - ip_payload
     tcp_header_fields = unpack(TCP_HEADER_FORMAT, tport_layer_packet[: 20])
-    tcp_header = dict(zip(KEYS_TCP_FIELDS, tcp_header_fields))
+    tcp_headers = dict(zip(KEYS_TCP_FIELDS, tcp_header_fields))
 
     # Validate presence of any TCP options
     # 1. Shift offset 4 bits from data offset field and get no. of words value
-    options_offset = tcp_header['data_offset'] >> 4
+    options_offset = tcp_headers['data_offset'] >> 4
     tcp_options = None
 
     # If this offset is = 5 words means that Options and Padding fields are empty, so..
@@ -211,29 +219,24 @@ def unpack_tcp_fields(tport_layer_packet):
         # ? Extract MSS - WTF should I do with it?
         tcp_options = tport_layer_packet[20 : 4 * options_offset]
 
-    net_layer_packet = tport_layer_packet[4 * options_offset :]
+    payload = tport_layer_packet[4 * options_offset :]
 
     # Validate: if packet is headed towards the correct destination port
-    if (tcp_header['dest_port'] != TCP_SOURCE_PORT):
+    if (tcp_headers['dest_port'] != TCP_SOURCE_PORT):
         # TODO: Exit gracefully
         pass
 
     # Validate: TCP packet checksum - compute checksum again and add with the tcp checksum - should be 0xffff
-    pseudo_ip_header = pack(
-        PSEUDO_IP_HEADER_FORMAT, 
-        IP_SRC_ADDRESS, IP_DEST_ADDRESS, IP_PADDING, IP_PROTOCOL, len(tport_layer_packet)
-    )
-
-    if (not validate_header_checksum(tcp_header['checksum'], tport_layer_packet, pseudo_ip_header, tcp_options, net_layer_packet)):
+    if (not validate_header_checksum(tcp_headers['checksum'], tcp_headers, tport_layer_packet, tcp_options, payload)):
         # TODO: Throw some error or some shit
         pass
 
     # Return the Network layer packet adn TCP headers
-    return tcp_header, net_layer_packet
+    return tcp_headers, payload
 
 """
 Helper method to instantiate IP fields: Takes in tcp packet as param.
     param: tcp_packet
 """
-def pack_ip_fields(tcp_packet):
-    return
+def pack_ip_fields(tport_layer_packet):
+    pass
